31cd6775a10456105a4dd903381c253630190b94
Jace Benson <jace.benson@protonmail.com>
2020-04-02 08:52:06 -0500

    working on local machine, lots of updates

---
+++ scripts/build.js
@@ -0,0 +1,7 @@
+var rssFeeds = require('./buildnews');
+var communityBlogs = require('./buildsnblogs');
+var javiera = require('./buildsnjaviera');
+var markr = require('./buildsnmarkr');
+module.exports = {
+
+};
