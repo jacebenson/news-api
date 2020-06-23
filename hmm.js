b7918ed80d0adc4ec9893ad394403eef0a45757d
Jace Benson <jace.benson@gmail.com>
2020-06-23 10:20:59 -0500

    adding k17,k18,k19 links

--- hmm.js
+++ hmm.js
@@ -1,20 +1,27 @@
-
-$$('ytd-playlist-panel-video-renderer > a').forEach(function (element) {
+var arr = [];
+    $$('ytd-playlist-panel-video-renderer > a').forEach(function (element) {
     console.log(element.href);
     var videoURL = new URL(element.href);
     var videoURLParams = videoURL.searchParams;
     var videoID = videoURLParams.get('v')
     console.log(videoURLParams.get('v'));
     var noembedURL = 'https://noembed.com/embed?url=http://www.youtube.com/watch?v=' + videoID;
-
-    //http = new XMLHttpRequest()
-
-    //http.open("GET", noembedURL)
-    //http.send()
-
-    //http.onload = () => console.log(http.responseText)
+    /**
+     * ,{
+    "date": "2020-01-20T13:05:07.000Z",
+    "site": "ServiceNow HI Videos",
+    "category": "YouTube",
+    "title": "Troubleshooting a Failed Discovery Part 2 of 5: Scanning Phase",
+    "author": "",
+    "link": ""
+ },{
+     */
     fetch(noembedURL)
-        .then(async r => console.log(await r.text(), '... from then'))
+        .then(async r => (
+            //console.log(await (r.json()))
+            arr.push(await r.json())
+            )
+        )
         .catch(e => console.error('Boo...' + e));
 
     //No fear...
@@ -24,17 +31,42 @@ $$('ytd-playlist-panel-video-renderer > a').forEach(function (element) {
         )
     )();
 });
+var feed = arr.map(function(video){
+    return {
+        date: new Date().toISOString(),
+        site: "ServiceNow HI Videos",
+        category: "YouTube",
+        title: video.title,
+        author: video.author_name,
+        link: video.url
+    }
+})
+//https://hi.service-now.com/kb_view.do?sys_kb_id=d5da8270db749450f7fca851ca9619cf&sysparm_rank=16&sysparm_tsqueryId=dbbe41acdb291810f7fca851ca9619fc
 
-
-/*var xmlHttp = new XMLHttpRequest();
-xmlHttp.onreadystatechange = ProcessVideo;
-xmlHttp.open('GET', noembedURL, true);
-xmlHttp.send();
-}
-function ProcessVideo(){
-  if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
-    console.log(xmlHttp.response);
-  }
-}
-*/
-//'http://noembed.com/embed?url=http%3A//www.youtube.com/watch%3Fv%3DbDOYN-6gdRE&callback=my_embed_function'
+/**
+ * var arr = [];
+$$('.cm-content-header > a').forEach(function (element) {
+    //console.log($(element).text());
+    //console.log(element.href);
+    //console.log(element);
+    arr.push({
+        "date": "2019-05-05T00:00:00.000Z",
+        "site": "Knowledge 19",
+        "category": "Presentation",
+        "title": $(element).text(),
+        "author": "Miscellaneous",
+        "link": element.href
+    })
+    /**
+     * ,{
+    "date": "2020-01-20T13:05:07.000Z",
+    "site": "ServiceNow HI Videos",
+    "category": "YouTube",
+    "title": "Troubleshooting a Failed Discovery Part 2 of 5: Scanning Phase",
+    "author": "",
+    "link": ""
+    },{
+   
+});
+console.log(arr);
+ */
