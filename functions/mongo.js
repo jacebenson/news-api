c9bdc57069500d3923632ea29629a4d0c01aca51
Jace Benson <jace.benson@protonmail.com>
2021-02-15 09:42:11 -0600

    updating feeds

--- functions/mongo.js
+++ functions/mongo.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 57);
+/******/ 	return __webpack_require__(__webpack_require__.s = 61);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 57:
+/***/ 61:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
